18480d925f4d92e4c06dfbcc90fdbb852aa2a081
Gustavo Villaça <you@example.com>
2020-06-03 08:29:18 -0300

    Ajustes no chat, chamando API de chat

--- src/app/chatbot/chat-widget/chat-widget.component.ts
+++ src/app/chatbot/chat-widget/chat-widget.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { discardPeriodicTasks } from "@angular/core/testing";
+import { Component, OnInit, AfterViewChecked } from "@angular/core";
+import { ChatbotService } from "../chatbot.service";
 
 @Component({
   selector: "app-chat-widget",
   templateUrl: "./chat-widget.component.html",
   styleUrls: ["./chat-widget.component.css"],
 })
-export class ChatWidgetComponent implements OnInit {
-  constructor() {}
+export class ChatWidgetComponent implements OnInit, AfterViewChecked {
+  constructor(private chatService: ChatbotService) {}
 
   ngOnInit(): void {}
+
+  ngAfterViewChecked() {
+    window.scrollTo(0, document.body.scrollHeight);
+  }
 
   conversa: any[] = [];
 
@@ -31,25 +35,34 @@ export class ChatWidgetComponent implements OnInit {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async respostaBot(m) {
-    await this.delay(500);
-    if (m.msg == "Olá") {
+  async respostaBot(m: any) {
+    // await this.delay(100);
+
+    this.chatService.talk(m.msg).subscribe((ret: any) => {
       this.conversa.push({
         id: 2,
-        msg: "Olá, como vai você?",
+        msg: ret.result.fulfillment.speech,
         user: "Robô",
         data: Date.now(),
       });
-      return;
-    }
-    if (m.msg == "Qual o seu nome?") {
-      this.conversa.push({
-        id: 2,
-        msg: "Eu sou a Inteligencia Artificial AIB",
-        user: "BIA",
-        data: Date.now(),
-      });
-      return;
-    }
+    });
+    // if (m.msg == "Olá") {
+    //   this.conversa.push({
+    //     id: 2,
+    //     msg: "Olá, como vai você?",
+    //     user: "Robô",
+    //     data: Date.now(),
+    //   });
+    //   return;
+    // }
+    // if (m.msg == "Qual o seu nome?") {
+    //   this.conversa.push({
+    //     id: 2,
+    //     msg: "Eu sou a Inteligencia Artificial AIB",
+    //     user: "BIA",
+    //     data: Date.now(),
+    //   });
+    //   return;
+    // }
   }
 }
--- src/app/chatbot/chatbot.service.ts
+++ src/app/chatbot/chatbot.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+let msg = {
+  lang: "pt-br",
+  query: "",
+  sessionId: "12345",
+};
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: "Bearer e228c76aa6a54ce9b14a33b0980aacdf",
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatbotService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  talk(inputMsg: string) {
+    msg.query = inputMsg;
+    
+    return this.http.post(
+      "https://api.dialogflow.com/v1/query?v=20150910",
+      msg,
+      httpOptions
+    );
+  }
 }
